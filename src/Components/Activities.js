f2a965a7b84258b4375e3beaddd177c878f7e223
David Lu <davidlu@davids-mbp.attlocal.net>
2022-03-22 22:23:06 -0400

    Activities All showing

--- src/Components/Activities.js
+++ src/Components/Activities.js
@@ -6,23 +6,27 @@ const BASE_URL = 'https://fitnesstrac-kr.herokuapp.com/api'
 const Activities = () => {
 const [activities, setActivities] = useState([])
 
-  const fetchActivties =  async () => {
+  const fetchActivities =  async () => {
     const resp = await fetch(`${BASE_URL}/activities`)
     const info = await resp.json();
-    setActivities(info.data.activities)
+    console.log(info)
+    if (resp.error) {
+      throw new Error(resp.error)
+    }
+    setActivities(info)
   }
   
   
   useEffect(() => {
-    fetchActivties();
+    fetchActivities();
   }, []);
   
   return (
   <>
   <h1>Activities Page</h1>
-  {activities.map((actvity) => (
-    <div id="activities" key={actvity._id}>
-      <Link to={`/activities/${actvity._id}`}>
+  {activities.map((activity) => (
+    <div id="activities" key={activity._id}>
+      <Link to={`/activities/${activity._id}`}>
         <h2>{activity.title}</h2>{" "}
       </Link>
       <h3>{activity.name}</h3>
