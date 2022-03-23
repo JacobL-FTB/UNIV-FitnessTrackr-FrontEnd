4ed6d5fad05fca1ccf13c9f6d9459cfc5bca26bb
David Lu <davidlu@davids-mbp.attlocal.net>
2022-03-22 22:03:01 -0400

    activities Unregistered done

--- src/Components/Activities.js
+++ src/Components/Activities.js
@@ -1,24 +1,39 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
+const BASE_URL = 'https://fitnesstrac-kr.herokuapp.com/api'
 
 const Activities = () => {
+const [activities, setActivities] = useState([])
 
   const fetchActivties =  async () => {
-    const resp = await fetch()
+    const resp = await fetch(`${BASE_URL}/activities`)
+    const info = await resp.json();
+    setActivities(info.data.activities)
   }
   
-  return 
+  
+  useEffect(() => {
+    fetchActivties();
+  }, []);
+  
+  return (
+  <>
   <h1>Activities Page</h1>
-  {actvities.map((actvity) => (
-    <div id="activities" key={actvities._id}>
-      <Link to={`/actvitiies/${actvities._id}`}>
+  {activities.map((actvity) => (
+    <div id="activities" key={actvity._id}>
+      <Link to={`/activities/${actvity._id}`}>
         <h2>{activity.title}</h2>{" "}
       </Link>
+      <h3>{activity.name}</h3>
       <p>{activity.description}</p>
-      <h3>{post.price}</h3>
+      
      
 
       
     </div>))}
+  </>
+  )
 };
 
 export default Activities;
