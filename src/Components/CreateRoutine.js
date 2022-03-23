49f19782da43bc1f9bf86ac82727c774dd773c9f
JacobL-FTB <jlarson2002@icloud.com>
2022-03-22 21:25:36 -0400

    Created/Imported CreateRoutine.js

---
+++ src/Components/CreateRoutine.js
@@ -0,0 +1,5 @@
+const CreateRoutine = () => {
+  return <h1>Create-Routine page</h1>;
+};
+
+export default CreateRoutine;
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import {
   My_Routines,
   Routines,
   Login_Register,
+  CreateRoutine,
 } from './Components/index';
 
 const Main = () => {
