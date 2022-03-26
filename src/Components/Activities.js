5bba9853034ec1a707d3da68a0ccf1fdc019a688
David Lu <davidlu@Davids-MacBook-Pro.local>
2022-03-26 12:47:52 -0400

    Activities

--- src/Components/Activities.js
+++ src/Components/Activities.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const BASE_URL = 'https://fitnesstrac-kr.herokuapp.com/api';
 
 const Activities = () => {
-  const [activities, setActivities] = useState([]);
-
+const [activities, setActivities] = useState([]);
+  
   const fetchActivities = async () => {
     const resp = await fetch(`${BASE_URL}/activities`);
     const info = await resp.json();
@@ -16,14 +16,22 @@ const Activities = () => {
     setActivities(info);
   };
 
+
+
   useEffect(() => {
     fetchActivities();
   }, []);
 
+  
+
   return (
     <>
-      <h1>Activities Page</h1>
+     
+     
+
+     
       {activities.map((activity) => (
+        
         <div id="activities" key={activity.id}>
           <Link to={`/activities/${activity.id}`}>
             <h2>{activity.title}</h2>{' '}
@@ -31,9 +39,11 @@ const Activities = () => {
           <h3>{activity.name}</h3>
           <p>{activity.description}</p>
         </div>
-      ))}
-    </>
+      ))} </>
+    
+    
   );
+ 
 };
 
 export default Activities;
